b6ad43493fe6af51bf6fea2bd48656b7518ca1ea
mattkjames7 <mattkjames7@gmail.com>
2021-03-12 13:01:38 +0000

    added submodule globals

---
+++ themissc/SST/_SST.py
@@ -0,0 +1,11 @@
+import numpy as np
+from .. import Globals
+
+#this just stores a few variables for this particular instrument
+
+#data path and index file name: format(Prod,L,sc)
+idxfname = Globals.DataPath + 'SST/{:s}.{:s}.{:s}.dat'
+datapath = Globals.DataPath + 'SST/{:s}/{:s}/{:s}/'
+
+#file version format
+vfmt = 'v\d'
