d8e40c62183f6f695a2ac43b3991c6e56ca27491
Dr Matt James <mattkjames7@gmail.com>
2026-08-17 23:41:03 +0100

    added field traces (#2)

---
+++ themissc/Trace/__init__.py
@@ -0,0 +1,7 @@
+"""Magnetic field tracing and daily footprint files."""
+
+from .ReadTrace import ReadTrace
+from .TraceField import TraceField
+from .UpdateParameters import UpdateParameters
+
+__all__ = ["ReadTrace", "TraceField", "UpdateParameters"]
